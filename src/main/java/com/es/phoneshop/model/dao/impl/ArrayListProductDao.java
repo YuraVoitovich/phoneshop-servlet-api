73ae7b94a83d018ef9062ba6b15f72a638ee9525
Yuri Voitovich <voitovich200233@gmail.com>
2023-01-04 00:58:48 +0300

    Task 1.5 fixed some minor issues

--- src/main/java/com/es/phoneshop/model/dao/impl/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/dao/impl/ArrayListProductDao.java
@@ -15,64 +15,69 @@ import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
 
-    private final ReadWriteLock lock = new ReentrantReadWriteLock();
     private final List<Product> products;
     private Long currentId = 1L;
+    private final Lock writeLock;
+    private final Lock readLock;
 
     public ArrayListProductDao() {
         this.products = new ArrayList<>();
+        ReadWriteLock lock = new ReentrantReadWriteLock();
+        this.writeLock = lock.writeLock();
+        this.readLock = lock.readLock();
     }
 
     @Override
-    public Product getProduct(@NonNull Long id) {
-        Lock readLock = lock.readLock();
+    public Product getProduct(Long id) {
+        if (id == null) throw new IllegalArgumentException("Id can not be null");
         readLock.lock();
         Product product = products.stream()
-                .filter(o -> id.equals(o.getId()))
-                .findAny().
-                orElseThrow(() -> new NoSuchProductException(String.format("Product with id=%s not found", id)));
+                .filter(currentProduct -> id.equals(currentProduct.getId()))
+                .findAny()
+                .orElseThrow(() -> new NoSuchProductException(String.format("Product with id=%s not found", id)));
         readLock.unlock();
         return product;
     }
 
     @Override
     public List<Product> findProducts() {
-        Lock readLock = lock.readLock();
         readLock.lock();
         List<Product> foundProducts =  products.stream()
-                .filter(o -> o.getPrice() != null)
-                .filter(o -> o.getStock() > 0)
+                .filter(currentProduct -> currentProduct.getPrice() != null)
+                .filter(currentProduct -> currentProduct.getStock() > 0)
                 .collect(Collectors.toList());
         readLock.unlock();
         return foundProducts;
     }
 
+    private void addProduct(Product product) {
+        product.setId(currentId++);
+        products.add(product);
+    }
     @Override
-    public void save(@NonNull Product product) {
-        Lock writeLock = lock.writeLock();
+    public void save(Product productToSave) {
+        if (productToSave == null) throw new IllegalArgumentException("Product to save can not be null");
         writeLock.lock();
-        if (product.getId() != null) {
-            Optional<Product> productOptional = products.stream()
-                    .filter(o -> o.getId().equals(product.getId()))
+        if (productToSave.getId() != null) {
+            Optional<Product> foundProductOptional = products.stream()
+                    .filter(currentProduct -> currentProduct.getId().equals(productToSave.getId()))
                     .findAny();
-            if (productOptional.isPresent()) {
-                products.set(products.indexOf(productOptional.get()), product);
+            if (foundProductOptional.isPresent()) {
+                products.set(products.indexOf(foundProductOptional.get()), productToSave);
             } else {
-                product.setId(currentId++);
-                products.add(product);
+                addProduct(productToSave);
             }
         } else {
-            product.setId(currentId++);
-            this.products.add(product);
+            addProduct(productToSave);
         }
         writeLock.unlock();
     }
 
     @Override
-    public void delete(@NonNull Long id) {
-        Lock writeLock = lock.writeLock();
+    public void delete(Long id) {
+        if (id == null) throw new IllegalArgumentException("Id can not be null");
         writeLock.lock();
-        if (!this.products.removeIf(o -> id.equals(o.getId())))
+        if (!this.products.removeIf(currentProduct -> id.equals(currentProduct.getId())))
             throw new NoSuchProductException(String.format("Product with id=%s not found for deleting", id));
         writeLock.unlock();
     }
