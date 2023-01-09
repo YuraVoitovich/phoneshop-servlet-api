1720934c8ddacf54e9a0e57d0347405f6e5d6680
Yuri Voitovich <voitovich200233@gmail.com>
2023-01-09 21:13:02 +0300

    Added product page

---
+++ src/main/java/com/es/phoneshop/web/ProductPageServlet.java
@@ -0,0 +1,31 @@
+package com.es.phoneshop.web;
+
+import com.es.phoneshop.model.dao.DAOProvider;
+import com.es.phoneshop.model.dao.ProductDao;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ProductPageServlet extends HttpServlet {
+
+    private ProductDao productDao;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        productDao = DAOProvider.getInstance().getProductDao();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String productId = request.getPathInfo().substring(1);
+        request.setAttribute("product", productDao.getProduct(Long.parseLong(productId)));
+        request.getRequestDispatcher("/WEB-INF/pages/product.jsp").forward(request, response);
+    }
+
+
+}
