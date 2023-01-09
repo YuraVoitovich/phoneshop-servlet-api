037371b109f2b42032cfaa7fdc5f1518b970ea50
Yuri Voitovich <voitovich200233@gmail.com>
2023-01-09 21:43:18 +0300

    Added Pair<K, L>

---
+++ src/main/java/com/es/phoneshop/utils/Pair.java
@@ -0,0 +1,11 @@
+package com.es.phoneshop.utils;
+
+public class Pair <K, V>{
+    public K fst;
+    public V snd;
+
+    public Pair(K fst, V snd) {
+        this.fst = fst;
+        this.snd = snd;
+    }
+}
