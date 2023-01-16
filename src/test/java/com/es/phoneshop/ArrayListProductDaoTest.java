16dd3ff26037f27eca9df2cf6fc9187b32d5acb0
Yuri Voitovich <voitovich200233@gmail.com>
2023-01-16 22:10:44 +0300

    Fixed some issues

--- src/test/java/com/es/phoneshop/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/ArrayListProductDaoTest.java
@@ -23,7 +23,7 @@ public class ArrayListProductDaoTest
 
     @Before
     public void setup() {
-        productDao = new ArrayListProductDao();
+        productDao = ArrayListProductDao.getInstance();
     }
 
     @Test
