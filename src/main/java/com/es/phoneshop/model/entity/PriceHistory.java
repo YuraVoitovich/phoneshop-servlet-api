094509f901aa4b174092a17aa714c30ddb9182c6
Yuri Voitovich <voitovich200233@gmail.com>
2023-01-24 02:10:46 +0300

    Made PriceHistory Serializable

--- src/main/java/com/es/phoneshop/model/entity/PriceHistory.java
+++ src/main/java/com/es/phoneshop/model/entity/PriceHistory.java
@@ -3,12 +3,13 @@ package com.es.phoneshop.model.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
-public class PriceHistory {
+public class PriceHistory implements Serializable {
     private BigDecimal price;
     private LocalDate date;
 }
